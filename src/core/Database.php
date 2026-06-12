9aa1fc095a436846d27f4c31f533d73f792b9b3e
Jitendra Kumar <104877854+igjitendra@users.noreply.github.com>
2026-06-12 08:27:12 +0530

    Implement singleton pattern for Database connection

---
+++ src/core/Database.php
@@ -0,0 +1,13 @@
+<?php
+class Database {
+    private static ?PDO $instance = null;
+
+    public static function getInstance(): PDO {
+        if (self::$instance === null) {
+            $config = require __DIR__ . '/../config/database.php';
+            $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}";
+            self::$instance = new PDO($dsn, $config['username'], $config['password'], $config['options']);
+        }
+        return self::$instance;
+    }
+}
